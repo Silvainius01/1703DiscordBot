54ddcf871713e9be0b288b046fb2c31b5876b2c0
Connor Adam <connor.adam@ymail.com>
2020-04-15 21:41:30 -0700

    Installed discord.py, first draft of event management done

--- src/EventManagment/CharacterData.py
+++ src/EventManagment/CharacterData.py
@@ -1,8 +1,53 @@
+from PsPlayerEvents import *
 
 class ExpAggregate:
-    expName = ""
-    expTotal = ""
-    expCount = ""
+    expName = "none"
+    expTotal = 0
+    expCount = 0
+
+    def __init__(self, name:str):
+        expName = name
+        expTotal = 0
+        expCount = 0
+        return
+    def AddTick(self, amt:int):
+        expTotal += amt
+        expCount += 1
+        return
 
 class CharacterData:
-    
+    expDict = {}
+    charId = ""
+    charName = ""
+    eventTypeDict = {
+        "AchievementEarned": [],
+        "BattleRankUp": [],
+        "Death": [],
+        "GainExperience": [],
+        "ItemAdded":[],
+        "PlayerFacilityCapture": [],
+        "PlayerFacilityDefend": [],
+        "PlayerLogin": [],
+        "PlayerLogout": [],
+        "SkillAdded": [],
+        "VehicleDestroy": [],
+    }
+
+    def __init__(self, id:str):
+        self.charId = id
+        self.charName = name
+        #init all known experience types
+        self.expDict["7"] = ExpAggregate("revive")
+        return
+
+    def AddExpTick(self, event:ExperienceGainEvent):
+        if not event.experience_id in self.expDict:
+            self.expDict[event.experience_id] = ExpAggregate(event.experience_id)
+        self.expDict[event.experience_id].AddTick(event.amount)
+
+    def AddEvent(self, event):
+        if event.event_name == "GainExperience":
+            self.AddExpTick(event)
+        if event.event_name in self.eventTypeDict:
+            self.eventTypeDict[event.event_name].append(event)
+        else: print("Attemtped to add untracked event type: " + event.event_name)
--- src/EventManagment/PsEventManager.py
+++ src/EventManagment/PsEventManager.py
@@ -1,13 +1,41 @@
 import json
+import sys
 from collections import namedtuple
-
-class BasePsEvent:
-    payload = ""
-    service = ""
-    type = ""
+from CharacterData import *
+   
 
 class EventManagerPS2:
-    i = 0
+    eventFuncDict = {
+        "ContinentLock": [],
+        "ContinentUnlock": [],
+        "FacilityControl": [],
+        "Metagame": [],
+    }
+    characterDict = {
+    }
     
+    def __init__(self):
+        return
+
+    def ReceiveEvents(json:str):
+
+        return
+
+    def ReceiveEvent(json: str):
+        baseEvent = json.loads(json, object_hook=BaseEventDecoder)
+        realEvent = EventDecoder(baseEvent.payload)
+        eventTypeDict[realEvent.event_name].append(realEvent)
+        return
+
     def BaseEventDecoder(eventDict):
        return namedtuple('BasePsEvent', eventDict.keys())(*eventDict.values)
+
+    def EventDecoder(payload):
+        name = payload.event_name + "Event"
+        return namedtuple(name, payload.keys())(*payload.values())
+
+    def AddEventToCharacter(event):
+        if not event.character_id in characterDict:
+            characterDict[event.character_id] = CharacterData(event.character_id)
+        characterDict[event.character_id].AddEvent(event)
+        return
